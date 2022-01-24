780d0f1fa018740af3a96c72e94ba19c97ad08e1
Konrad Urbańczyk <elearning.warsaw@gmail.com>
2022-01-24 20:05:35 +0100

    user slice working

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,28 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/assets/css/utils.css"
 import "./src/assets/css/global.css"
-import { onAuthStateChanged } from "firebase/auth"
-import { auth, database } from "./src/firebase/firebase.utils"
-import { onValue, ref } from "firebase/database"
-import { userActions } from "./src/store/store"
-import { useDispatch } from "react-redux"
+
 
 import wrapWithProvider from "./wrap-with-provider"
 export const wrapRootElement = wrapWithProvider
 
-const dispatch =useDispatch()
 
-export const onClientEntry = () => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      const uid = user.uid
-      const userRef = ref(database, "users/" + uid)
-      onValue(userRef, snapshot => {
-        const data = snapshot.val()
-        dispatch(userActions.setUser(data))
-      })
-    } else {
-      console.log("no user")
-    }
-  })
-}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,42 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { useState } from "react"
 import Sidebar from "./Sidebar"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth, database } from "../firebase/firebase.utils"
+import { onValue, ref } from "firebase/database"
+import { userActions } from "../store/store"
+import { useDispatch } from "react-redux"
 
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        const uid = user.uid
+        const userRef = ref(database, "users/" + uid)
+        onValue(userRef, snapshot => {
+          const data = snapshot.val()
+          dispatch(userActions.setUser(data))
+        })
+      } else {
+        console.log("no user")
+      }
+    })
+  }, [])
 
   const toggleSidebar = () => {
-    setIsOpen(prev=>!prev)
+    setIsOpen(prev => !prev)
   }
   return (
     <>
       <Navbar toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
-      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
     </>
   )
 }
